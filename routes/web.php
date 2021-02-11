45159ec6bd843b6ae74aa1b240d36193c9afc505
kemiranda <kjmd2s@gmail.com>
2021-02-10 21:21:07 -0500

    Generando entidades y relaciones con la database

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\App;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/welcome/{lang}', function ($lang) {
+    App::setLocale($lang);
+    return view('welcome')->with([
+        'lang' => $lang
+    ]);
+})->name('welcome');
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('welcome','en'));
 });
+
+// remove register option and password reset options and their routes
+//Auth::routes(['register' => false, 'reset' => false]);
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('/companies', 'CompanyController');
+
+// we cannot use a route resource, need to manually do the employees
+//Route::resource('/employees', 'EmployeeController');
+Route::get('/companies/{company}/employees','EmployeeController@index')->name('employees.index');
+Route::get('/companies/{company}/employees/create','EmployeeController@create')->name('employees.create');
+Route::post('/companies/{company}/employees/store','EmployeeController@store')->name('employees.store');
+Route::get('/companies/{company}/employees/{employee}/edit','EmployeeController@edit')->name('employees.edit');
+Route::put('/companies/{company}/employees/{employee}/update','EmployeeController@update')->name('employees.update');
+Route::delete('/companies/{company}/employees/{employee}/delete','EmployeeController@destroy')->name('employees.destroy');
